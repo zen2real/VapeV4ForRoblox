6282f54603fda5ba3376c4bc7c83e34cdd846094
zen2real <groffzach9@gmail.com>
2026-04-12 05:55:30 +0000

    j

--- games/6872274481.lua
+++ games/6872274481.lua
@@ -42040,7 +42040,7 @@ run(function()
     local MotionBlurConnection
     local MotionBlurEffect
 
-    MotionBlur = vape.Categories.CloudWare:CreateModule({
+    MotionBlur = vape.Categories.R:CreateModule({
         Name = 'MotionBlur',
         Function = function(callback)
             local Lighting = game:GetService("Lighting")
